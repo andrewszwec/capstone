a8f8e84d999e90c4e84483f4f154307ebbb172d0
kilaorange <andrew.szwec@gmail.com>
2015-10-11 22:13:52 +1100

    commit after quiz 2 completed

---
+++ girlsVsBoys.R
@@ -0,0 +1,7 @@
+# Names identification
+
+girls <- read.table("census-dist-female-first.txt", header = FALSE, sep="\t")
+
+#put girls and boys names into a table with a col that says if girl or boy
+# then join to the user table to get gender in the user table
+# then figure out the distribution of boys and girls?
